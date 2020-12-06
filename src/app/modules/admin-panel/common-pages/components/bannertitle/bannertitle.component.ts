8e51f1f08b76e7de9b8fa327bed9a6eda7cc2a0e
sbhushan@smartdatainc.com <sbhushan@smartdatainc.net>
2020-12-06 19:37:06 +0530

    chnages

--- src/app/modules/admin-panel/common-pages/components/bannertitle/bannertitle.component.ts
+++ src/app/modules/admin-panel/common-pages/components/bannertitle/bannertitle.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bannertitle.component.css']
 })
 export class BannertitleComponent implements OnInit {
-
+  editorConfig = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig2 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
   constructor() { }
 
   ngOnInit() {
--- src/app/modules/admin-panel/common-pages/components/whychoose/whychoose.component.ts
+++ src/app/modules/admin-panel/common-pages/components/whychoose/whychoose.component.ts
@@ -6,7 +6,110 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./whychoose.component.css']
 })
 export class WhychooseComponent implements OnInit {
-
+  editorConfig = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig2 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig3 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig4 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig5 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig6 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig7 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
+  editorConfig8 = {
+    editable: true,
+    spellcheck: false,
+    height: '10rem',
+    width: '35rem',
+    minHeight: '5rem',
+    placeholder: 'Type something. Test the Editor... ',
+    translate: 'no',
+    "toolbar": [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent"],
+    ],
+  };
   constructor() { }
 
   ngOnInit() {
